7c80abfc90e8f24b5db43cf5222d687bd1678fe3
miarosario <131704864+miarosario@users.noreply.github.com>
2023-09-25 13:23:49 -0500

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,22 @@
+var nombre = prompt("Escribe tu nombre  ");
+
+var respuesta1 = prompt("1. ¿Cuándo se realizará la inauguración de las Olimpiadas? \nA: Lunes 02 de octubre\nB:Viernes 29 de setiembre \nC: Jueves 28 de setiembre ");
+
+var respuesta2 = prompt("2. ¿De qué color es la prompoción de 2do de secundaria? \nA: Celeste o Turquesa \nB: Rojo \nC: Morado");
+
+var respuesta3 = prompt("3. ¿Nuevo deporte incluido este año? \nA: Futsal Femenino\nB: Balonmano\nC: Marcha atletica")
+
+if (respuesta1 == "B"){
+    var puntaje == puntaje + 25;
+}
+
+if (respuesta2 == "A"){
+    var puntaje == puntaje + 25;
+}
+
+if (respuesta3 == "C"){
+    var puntaje == puntaje + 25;
+}
+
+f( puntaje <= 50)
+   document.write(nombre + " has logrado: " + puntaje + " FELICITACIONES")
